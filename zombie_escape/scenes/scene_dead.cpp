48ea51c165d61890ddc57d53ed960b59a4482a29
Mihail Benev <mihail.benev99@gmail.com>
2021-05-01 15:31:35 +0300

    Tidied up the game over screen

--- zombie_escape/scenes/scene_dead.cpp
+++ zombie_escape/scenes/scene_dead.cpp
@@ -13,20 +13,18 @@ void DeadScene::Load() {
     auto txt = makeEntity();
     // Text on screen
     auto deadText = txt->addComponent<TextComponent>("You have joined the horde!");
-    deadText->SetPosition(Vector2f(200.0f, 100.0f));
+    deadText->SetPosition(Vector2f(Engine::GetWindow().getSize().x/2-300, Engine::GetWindow().getSize().y/2-200));
     auto backToMenu = txt->addComponent<TextComponent>("Press P to go back to menu.");
-    backToMenu->SetPosition(Vector2f(200.0f, 150.0f));
+    backToMenu->SetPosition(Vector2f(Engine::GetWindow().getSize().x/2-300, Engine::GetWindow().getSize().y/2));
   }
   setLoaded(true);
 }
 void DeadScene::UnLoad() { Scene::UnLoad(); }
 void DeadScene::Update(const double& dt) {
-     // Change resolution based on user input
+     // Go back to menu
     if (sf::Keyboard::isKeyPressed(Keyboard::P)) {
         Engine::GetWindow().setView(sf::View(sf::FloatRect(0, 0, Engine::GetWindow().getSize().x, Engine::GetWindow().getSize().y)));
         Engine::ChangeScene(&menu);
     }
-    
-
   Scene::Update(dt);
 }
