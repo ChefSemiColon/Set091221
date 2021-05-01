c93648ff6155531e1e359eb7c9452e966eb423a2
Mihail Benev <mihail.benev99@gmail.com>
2021-05-01 21:35:12 +0300

    Get a random score
    Created a method that generates a random score from 3 values based on enemies killed.

--- zombie_escape/scenes/scene_dead.cpp
+++ zombie_escape/scenes/scene_dead.cpp
@@ -7,17 +7,47 @@
 using namespace std;
 using namespace sf;
 
+// Get a random score from 3 values
+int randomScore(int killCount, int one, int two, int three)
+{
+    int randomScore = 0;
+    if (killCount == 0){}
+    else
+    {
+        for (int i = 0; i < killCount; i++)
+        {
+            int randomNumber = 0;
+            int randomiser = rand() % 3;
+            switch (randomiser) {
+            case 0:
+                randomNumber = one;
+                break;
+            case 1:
+                randomNumber = two;
+                break;
+            case 2:
+                randomNumber = three;
+                break;
+            }
+            randomScore += randomNumber;
+        }
+    }
+    return randomScore;
+}
+
 void DeadScene::Load() {
   cout << "Game over screen Load \n";
   {
     auto txt = makeEntity();
 
-    string score = "Score:   " + to_string(enemiesKilled * 2);
+    string score = "Score: " + to_string(randomScore(enemiesKilled, 50, 75, 100)) + "    ||    Numbers of enemies killed: " + to_string(enemiesKilled);
+    // Reset the enemies killed counter
+    enemiesKilled = 0;
     // Text on screen
     auto deadText = txt->addComponent<TextComponent>("You have joined the horde!");
     deadText->SetPosition(Vector2f(Engine::GetWindow().getSize().x/2-300, Engine::GetWindow().getSize().y/2-200));
     auto scoreText = txt->addComponent<TextComponent>(score);
-    scoreText->SetPosition(Vector2f(Engine::GetWindow().getSize().x / 2 - 300, Engine::GetWindow().getSize().y / 2 - 100));
+    scoreText->SetPosition(Vector2f(Engine::GetWindow().getSize().x / 2 - 500, Engine::GetWindow().getSize().y / 2 - 100));
     auto backToMenu = txt->addComponent<TextComponent>("Press P to go back to menu.");
     backToMenu->SetPosition(Vector2f(Engine::GetWindow().getSize().x/2-300, Engine::GetWindow().getSize().y/2));
   }
