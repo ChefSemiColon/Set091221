746fa71280abb3bd8dc96b63766e8c4ed3a5ee56
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-05-02 14:58:07 +0100

    Updated bullet physics
    Updated bullets so that they can now stop on walls.

--- zombie_escape/components/cmp_bullet.cpp
+++ zombie_escape/components/cmp_bullet.cpp
@@ -1,6 +1,7 @@
 #include"cmp_bullet.h"
 #include <engine.h>
 #include "../game.h"
+#include "LevelSystem.h"
 using namespace std;
 using namespace sf;
 
@@ -14,7 +15,10 @@ void Bullet::fire(Vector2f dir, Vector2f pos){
 
 void Bullet::update(double dt) {
     if (_parent->isAlive()) {
-        _parent->setPosition(_parent->getPosition()+ bulletVel *float(dt));
+
+        auto pos = _parent->getPosition() + bulletVel * float(dt);
+        if (ls::getTileAt(pos + Vector2f(10.5f, 10.5f)) != ls::WALL && ls::getTileAt(pos - Vector2f(10.5f, 10.5f)) != ls::WALL) {
+        _parent->setPosition(pos);
         for (int i = 0; i < numEnemiesAlive; ++i)
             if (length(enemies[i]->getPosition() - _parent->getPosition()) < 20.0f) {
                 enemies[i]->setPosition({ -1000.0f,-1000.0f });
@@ -26,6 +30,12 @@ void Bullet::update(double dt) {
                 enemiesKilled++;
                 break;
             }
+        }
+        else {
+            _parent->setAlive(false);
+            _parent->setVisible(false);
+        }
+
     }
 }
 
